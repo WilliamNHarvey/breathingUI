1ff01fd0590661a1d7cf4a49a712f30bf759c0bc
WilliamNHarvey <williamnharvey@gmail.com>
2018-02-01 23:34:20 -0500

    halfway to breathing ui

--- frontend/js/sleepear/controllers/sleepearController.js
+++ frontend/js/sleepear/controllers/sleepearController.js
@@ -8,5 +8,40 @@ define([
       $scope.switchPage = $rootScope.switchPage;
       $scope.location = /[^/]*$/.exec($location.path())[0];
 
+      var down = false;
+
+      var max = $(window).height();
+
+      var grower;
+      $('body').keydown(function(e) {
+          if(e.keyCode == 32){
+              down = true;
+
+              grower = setTimeout(function() {
+                  //var newheight = $('circle-1').height() + $('circle-1').height() * 0.02
+                  var newheight = max/(1 + 2.718^(-0.02($('circle-1').height())))
+                  $('circle-1').css({
+                      height: newheight,
+                      width: newheight
+                  })
+              }, 100);
+
+              $('body').keyup(function(u) {
+                  if(e.keyCode == 32 && down) {
+                      clearTimeout(grower);
+                      down = false;
+                      $('body').unbind("keyup")
+                      $('circle-1').css({
+                          height: "25px",
+                          width: "25px"
+                      })
+                  }
+
+                  else if (!down) {
+                      $('body').unbind("keyup")
+                  }
+              });
+          }
+      });
   });
 });
