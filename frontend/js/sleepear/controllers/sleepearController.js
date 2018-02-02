1cc1c9c0291307f94a1101c036c91b6bd24506ca
WilliamNHarvey <williamnharvey@gmail.com>
2018-02-01 23:41:12 -0500

    halfway to breathing ui

--- frontend/js/sleepear/controllers/sleepearController.js
+++ frontend/js/sleepear/controllers/sleepearController.js
@@ -19,8 +19,9 @@ define([
 
               grower = setTimeout(function() {
                   //var newheight = $('circle-1').height() + $('circle-1').height() * 0.02
-                  var newheight = max/(1 + 2.718^(-0.02 * ($('circle-1').height())));
+                  var newheight = max/(1 + 2.718^(-0.02 * ($('circle-1').height() - max/2)));
                   console.log(newheight);
+                  console.log($('circle-1').height());
                   $('.circle-1').css({
                       height: newheight,
                       width: newheight
