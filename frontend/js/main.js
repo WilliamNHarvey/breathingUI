af57e190371a8df0f1c262403999a4cd4b52559b
WilliamNHarvey <williamnharvey@gmail.com>
2018-02-05 16:15:58 -0500

    chart

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,10 +2,10 @@ require.config({
     baseUrl : 'js',
     paths : {
         // AngularJS
-        'angular'               : '//ajax.googleapis.com/ajax/libs/angularjs/1.2.22/angular',
-        'angular.resource'    : '//ajax.googleapis.com/ajax/libs/angularjs/1.2.22/angular-resource.min',
-        'angular.animate'     : '//ajax.googleapis.com/ajax/libs/angularjs/1.2.22/angular-animate.min',
-        'angular.sanitize'    : '//ajax.googleapis.com/ajax/libs/angularjs/1.2.22/angular-sanitize.min',
+        'angular'               : '//ajax.googleapis.com/ajax/libs/angularjs/1.6.7/angular.min',
+        'angular.resource'    : '//ajax.googleapis.com/ajax/libs/angularjs/1.6.7/angular-resource.min',
+        'angular.animate'     : '//ajax.googleapis.com/ajax/libs/angularjs/1.6.7/angular-animate.min',
+        'angular.sanitize'    : '//ajax.googleapis.com/ajax/libs/angularjs/1.6.7/angular-sanitize.min',
         'angular.pt-br'       : '//cdnjs.cloudflare.com/ajax/libs/angular-i18n/1.2.15/angular-locale_pt-br',
 
         // AngularAMD
