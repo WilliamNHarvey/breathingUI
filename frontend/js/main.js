e0b24ada3af8765407cb8f4126f95c737cbfff74
WilliamNHarvey <williamnharvey@gmail.com>
2018-02-05 16:52:34 -0500

    chart

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,10 +2,10 @@ require.config({
     baseUrl : 'js',
     paths : {
         // AngularJS
-        'angular'               : '//ajax.googleapis.com/ajax/libs/angularjs/1.6.7/angular.min',
-        'angular.resource'    : '//ajax.googleapis.com/ajax/libs/angularjs/1.6.7/angular-resource.min',
-        'angular.animate'     : '//ajax.googleapis.com/ajax/libs/angularjs/1.6.7/angular-animate.min',
-        'angular.sanitize'    : '//ajax.googleapis.com/ajax/libs/angularjs/1.6.7/angular-sanitize.min',
+        'angular'               : '//ajax.googleapis.com/ajax/libs/angularjs/1.2.22/angular.min',
+        'angular.resource'    : '//ajax.googleapis.com/ajax/libs/angularjs/1.2.22/angular-resource.min',
+        'angular.animate'     : '//ajax.googleapis.com/ajax/libs/angularjs/1.2.22/angular-animate.min',
+        'angular.sanitize'    : '//ajax.googleapis.com/ajax/libs/angularjs/1.2.22/angular-sanitize.min',
         'angular.pt-br'       : '//cdnjs.cloudflare.com/ajax/libs/angular-i18n/1.2.15/angular-locale_pt-br',
 
         // AngularAMD
@@ -17,7 +17,6 @@ require.config({
         'angular-ui-router'   : '../lib/angular-ui-router/release/angular-ui-router.min',
         'checklist-model'     : '../lib/checklist-model/checklist-model',
         'angular-flash'       : '../lib/angular-flash/dist/angular-flash.min',
-
 
         // jQuery
         'jquery'              : '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min',
