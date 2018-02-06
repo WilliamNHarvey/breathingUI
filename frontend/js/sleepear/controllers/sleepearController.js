9b11dfbd986bcef88e20bfb1b6076d6bfc07610c
WilliamNHarvey <williamnharvey@gmail.com>
2018-02-06 13:53:37 -0500

    chart

--- frontend/js/sleepear/controllers/sleepearController.js
+++ frontend/js/sleepear/controllers/sleepearController.js
@@ -3,7 +3,7 @@ define([
   ], function(app) {
   'use strict';
 
-  app.controller('breathsController', function($rootScope, $scope, $location, $window) {
+  app.controller('breathsController', function($rootScope, $scope, $location, $window, d3) {
       window.ondragstart = function() { return false; };
       $scope.switchPage = $rootScope.switchPage;
       $scope.location = /[^/]*$/.exec($location.path())[0];
