b7cb2c2181d24e8bcc105dffb49312ba488b2536
WilliamNHarvey <williamnharvey@gmail.com>
2018-02-22 03:47:31 -0500

    sess

--- routes/api/user.js
+++ routes/api/user.js
@@ -64,8 +64,8 @@ router.route('/user')
           req.session.save();
           sessionStore.destroy(user.session, function(){
               User.update({_id: user._id}, {$set:{"session" : sid}});
-          }
-          res.cookie('SleepEarSess'+user._id, sid, { maxAge: 60000, httpOnly: true })
+          });
+          res.cookie('SleepEarSess'+user._id, sid, { maxAge: 60000, httpOnly: true });
           res.status(200);
           res.json({ message: "Login successful", user: { name: user.name, email: user.email, job: user.job } });
       }
