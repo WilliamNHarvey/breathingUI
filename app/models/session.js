a895a5a90d1c40413c5e022cb02ffd15df55051c
WilliamNHarvey <williamnharvey@gmail.com>
2018-02-22 19:22:13 -0500

    not saving over

--- app/models/session.js
+++ app/models/session.js
@@ -2,7 +2,7 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var SessionSchema   = new Schema({
-    _id         : { type: String , required: true, unique: true },
+    _id         : { type: String , required: true },
     cookie      : { type: String, required: true, dropDups: true },
     expires     : { type: Date , required: true },
     user        : { type: String, unique: true }
