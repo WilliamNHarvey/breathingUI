9efd58d9fdfa391cc8fe4404cec7e27e4e7fb85f
WilliamNHarvey <williamnharvey@gmail.com>
2018-03-27 10:55:12 -0400

    manage

--- frontend/js/sleepear/controllers/technicianController.js
+++ frontend/js/sleepear/controllers/technicianController.js
@@ -274,8 +274,8 @@ define([
                       bottom: 50,
                       left: 50
                   },
-                  height = 170 - margin.top - margin.bottom,
-                  width = 650 + data.length/248 - margin.left - margin.right;
+                  height = 500 - margin.top - margin.bottom,
+                  width = 1000 + data.length/248 - margin.left - margin.right;
 
               var x = d3.time.scale()
                   .domain(d3.extent(data, function(d) {
