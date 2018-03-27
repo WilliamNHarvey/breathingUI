f0add2f5737380fd3f331cb20df35c3d1c7d5a47
WilliamNHarvey <williamnharvey@gmail.com>
2018-03-27 15:43:05 -0400

    show labels for techy

--- frontend/js/sleepear/controllers/technicianController.js
+++ frontend/js/sleepear/controllers/technicianController.js
@@ -177,7 +177,7 @@ define([
                       bottom: 50,
                       left: 50
                   },
-                  height = 170 - margin.top - margin.bottom,
+                  height = 370 - margin.top - margin.bottom,
                   width = 650 + data.length/248 - margin.left - margin.right;
 
               var x = d3.time.scale()
